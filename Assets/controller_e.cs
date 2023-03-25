87ccc0e03a1e34e9c99b7fce0d639e4e8ef62105
elonezra <elonezra1995@hotmail.com>
2023-03-25 23:24:09 +0300

    init

---
+++ Assets/controller_e.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller_e : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 1.0f;// step size of the object
+
+    [SerializeField]
+    InputAction moveUp = new InputAction(typ: InputActionTrpe.Button);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
